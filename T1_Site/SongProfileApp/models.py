5295b0d67e639a56f26d6f3bf134da52276ce3fe
brunomikmenta <118640165+brunomikmenta@users.noreply.github.com>
2026-04-09 10:11:29 -0300

    Create app, create user model

---
+++ T1_Site/SongProfileApp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+GENDER_CHOICES = [
+    ('M', 'Masculino'),
+    ('F', 'Feminino'),
+    ('O', 'Outro'),
+    ('N', 'Prefiro não dizer'),
+]
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30)
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    gender = models.ChoiceField(max_length=1, choices = GENDER_CHOICES)
+    email = models.EmailField(max_length=254, unique=True)
